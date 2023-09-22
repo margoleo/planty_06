983116d5e6911a924889e69baf4a3f1aa077b0b8
margoleo <margauxleonet@gmail.com>
2023-09-22 21:12:54 +0200

    Initialisation

---
+++ wp-content/themes/blankslate-child/footer.php
@@ -0,0 +1,11 @@
+</main>
+<?php get_sidebar(); ?>
+</div>
+<footer class="planty_footer">
+<div class="mentions_legales" > 
+    <a href=" "> Mentions légales </a> </div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/blankslate-child/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="wrapper" class="hfeed">
+<header id="header" role="banner" class="planty_header">
+    
+<div> 
+<img src="blankslate-child/img/logo.svg" alt="Logo">
+
+<nav id="menu" class="planty_nav" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+</nav>
+
+</div>
+</header>
+<div id="container">
+<main id="content" role="main">
